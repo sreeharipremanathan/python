d63db09cfb22865f3f7ba2864c6c085ef2d9d909
sreeharipremanathan <sreeharipremanathan@gmail.com>
2024-08-12 13:41:02 +0530

    python

---
+++ sample.py
@@ -0,0 +1,35 @@
+print('hey')
+# intiger
+num_int=10
+num_float=10.10
+num_cmplex=12+4j
+
+# string
+s='hey'
+
+# boolian
+bool=True
+bool1=False
+
+# sequence
+list=[10,11,12]
+tuple=(10,15,20)
+
+# set
+set={22,25,35}
+
+# mapping type
+
+map={'name':'hari','age':22}
+print(type(num_int))
+print(type(num_float))
+print(type(num_cmplex))
+print(type(s))
+print(type(bool))
+print(type(bool1))
+print(type(list))
+print(type(tuple))
+print(type(set),type(map))
+
+cast=float(num_int)
+print(type(cast))
