16982f296023c25db7c8016255e8a953c00898af
sreeharipremanathan <sreeharipremanathan@gmail.com>
2024-08-13 13:42:22 +0530

    operators

---
+++ input.py
@@ -0,0 +1,6 @@
+name=input("enter your name : ")
+age=int(input("enter your age : "))
+weight=float(input("enter your weight : "))
+print("name : ",name)
+print("age : ",age)
+print("weight : ",weight)
