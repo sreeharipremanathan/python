0d4d697b498baa469c0a61a560b68f7a79f7cd2e
sreeharipremanathan <sreeharipremanathan@gmail.com>
2024-08-13 14:03:14 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,67 @@
+# arithmetic
+
+print(20+10)
+print("sree"+"hari")
+
+print(20-10)
+
+print(2*5)
+print("hari "*4)
+
+print(20/5)
+
+print(10%3)
+
+print(20//3)
+
+print(10**2)
+
+# assignment
+
+a=11
+print(a)
+
+a+=4
+print(a)
+
+a-=10
+print(a)
+
+a*=2
+print(a)
+
+a/=2
+print(a)
+
+a=10
+a//=3
+print(a)
+
+a=10
+a%= 2
+print(a)
+a=10
+a**=2
+print(a)
+
+# comparison operators
+
+print(10==10)
+print(10==11)
+
+print(10!=10)
+print(10!=11)
+
+print(10>11)
+print(10>9)
+
+print(10<11)
+print(10<9)
+
+print(10>=11)
+print(10>=9)
+
+print(10<=11)
+print(10<=9)
+
+
