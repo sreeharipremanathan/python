46c93c4e4226d49f1d2ca9ef1f60f30d2b2e7f6e
sreeharipremanathan <sreeharipremanathan@gmail.com>
2024-08-14 13:49:18 +0530

    operators and string

--- operators.py
+++ operators.py
@@ -1,67 +1,163 @@
 # arithmetic
 
-print(20+10)
-print("sree"+"hari")
+# print(20+10)
+# print("sree"+"hari")
 
-print(20-10)
+# print(20-10)
 
-print(2*5)
-print("hari "*4)
+# print(2*5)
+# print("hari "*4)
 
-print(20/5)
+# print(20/5)
 
-print(10%3)
+# print(10%3)
 
-print(20//3)
+# print(20//3)
 
-print(10**2)
+# print(10**2)
 
 # assignment
 
-a=11
-print(a)
+# a=11
+# print(a)
 
-a+=4
-print(a)
+# a+=4
+# print(a)
 
-a-=10
-print(a)
+# a-=10
+# print(a)
 
-a*=2
-print(a)
+# a*=2
+# print(a)
 
-a/=2
-print(a)
+# a/=2
+# print(a)
 
-a=10
-a//=3
-print(a)
+# a=10
+# a//=3
+# print(a)
 
-a=10
-a%= 2
-print(a)
-a=10
-a**=2
-print(a)
+# a=10
+# a%= 2
+# print(a)
+# a=10
+# a**=2
+# print(a)
 
 # comparison operators
 
-print(10==10)
-print(10==11)
+# print(10==10)
+# print(10==11)
 
-print(10!=10)
-print(10!=11)
+# print(10!=10)
+# print(10!=11)
 
-print(10>11)
-print(10>9)
+# print(10>11)
+# print(10>9)
 
-print(10<11)
-print(10<9)
+# print(10<11)
+# print(10<9)
 
-print(10>=11)
-print(10>=9)
+# print(10>=11)
+# print(10>=9)
 
-print(10<=11)
-print(10<=9)
+# print(10<=11)
+# print(10<=9)
 
+# logical operators
 
+# and
+
+# print(10==10 and 10>=8) 
+# # true
+# print(10==10 and 10>=11)
+# # false
+# print(10==11 and 10>=9)
+# # false
+# print(10==11 and 10>=11)
+# # false
+
+# or
+
+# print(10==10 or 10>=9)
+# # true
+# print(10==11 or 10>=9)
+# # true
+# print(10==10 or 10>=11)
+# # true
+# print(10==11 or 10>=11)
+# # false
+
+# not
+
+# print(not(10==10))
+# # false
+# print(not(10==11))
+# # true
+
+# membership operators
+
+# in
+
+# a="hari"
+# print("r" in a)
+# # true
+
+# b="s"
+# print(b in a)
+# # false
+
+# # not in
+
+# print(b not in a)
+# # true
+# c='h'
+# print(c not in a)
+# # false
+# print("h" not in a)
+# # false
+
+# print('y' in 'python' and 'o' in 'python')
+
+# identify operators
+
+# is
+
+# a=10
+# b=10
+# print(id(a))
+# print(id(b))
+
+# print(a==b)
+# print(a is b)
+
+# l=[10,11,12]
+# li=[10,11,12]
+# print(id(l))
+# print(id(li))
+# print(l==li)
+# print(l is li)
+
+# is not
+
+# a=10
+# b=10
+# print(id(a))
+# print(id(b))
+# print(a is not b)
+
+# l=[10,11,12]
+# li=[10,11,12]
+# print(id(l))
+# print(id(li))
+# print(l is not li)
+
+a='hari'
+b='sree'
+print(id(a))
+print(id(b))
+print(a==b)
+print(a is b)
+print(a is not b)
+print(id('sree'))
+print(id('hari'))
---
+++ string.py
@@ -0,0 +1,6 @@
+a='hey "im" hari'
+print(len(a))
+a='''hey
+im here'''
+print(a)
+print(a[-1])
