ec4e5eba7eebe038291d0ed45d13530687c73240
sreeharipremanathan <sreeharipremanathan@gmail.com>
2024-08-16 13:39:32 +0530

    if

---
+++ if.py
@@ -0,0 +1,48 @@
+# a=10
+# b=11
+# if a==b:
+#     print('it is equal')
+# else:
+#     print("it's not equal")
+
+# a=int(input('enter a number :'))
+# b=int(input('enter a numner :'))
+# if a>b:
+#     print('largest number is',a)
+# else:
+#     print('largest number is',b)
+
+
+# age=int(input('enter your age :'))
+# if age>=18:
+#     print('you are eligible to vote' )
+# else:
+#     print('you are not eligible')
+
+# num=int(input('enter a value :'))
+# if num%2==0:
+#     print('even')
+# else:
+#     print('odd')
+
+# wrd=str(input('enter a word :'))
+# ltr=str(input('enter a letter :'))
+# if ltr in wrd:
+#     print('given letter is present in the word')
+# else:
+#     print('given letter is not present in the word')
+
+# salary=int(input('enter your salary :'))
+# year=int(input('year of service :'))
+# if year>=5:
+#     bonus=salary*5/100
+#     print('your bonus is:',bonus)
+# else:
+#     print('you are not eligible for bonus!!')
+
+no=int(input('enter a number :'))
+rem=no%10
+if rem%3==0:
+    print('the digit is divisible')
+else:
+    print('the digit is not divisible')
