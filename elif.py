646cfb9a3002f150707eecb37c2e04bd426c32fe
sreeharipremanathan <sreeharipremanathan@gmail.com>
2024-08-17 13:27:20 +0530

    elif,nestedif

---
+++ elif.py
@@ -0,0 +1,61 @@
+# a=int(input('enter 1st number :'))
+# b=int(input('enter 2nd number :'))
+# if a==b:
+#     print('equal')
+# elif a>b:
+#     print(a)
+# else:
+#     print(b)
+
+# day=int(input('enter a number :'))
+# if day==1:
+#     print('sunday')
+# elif day==2:
+#     print('monday')
+# elif day==3:
+#     print('tuesday')
+# elif day==4:
+#     print('wednesday')
+# elif day==5:
+#     print('thursday')
+# elif day==6:
+#     print('friday')
+# elif day==7:
+#     print('saturday')
+# else:
+#     print('invalid entry')
+
+# city=input('enter the city :')
+# if city=='delhi':
+#     print('red fort')
+# elif city=='agra':
+#     print('taj mahal')
+# elif city=='jaipur':
+#     print('jal mahal')
+# else:
+#     print('invalid entry')
+
+# cost=int(input('enter your amount :'))
+# if cost>100000:
+#     tax=cost*0.15
+#     print("you'll get",tax)
+# elif cost>50000 and cost<=100000:
+#     tax=cost*0.10
+#     print("you'll get",tax)
+# elif cost<=50000:
+#     tax=cost*0.05
+#     print("you'll get",tax)
+# else:
+#     print('invald')
+
+# unit=int(input('enter your unit :'))
+# if unit<=100:
+#     print('there is no charge')
+# elif unit<=200:
+#     bill=(unit-100)*5
+#     print(bill)
+# elif unit>200:
+#     bill=((unit-200)*10)+500
+#     print(bill)
+# else:
+#     print('invalid')
