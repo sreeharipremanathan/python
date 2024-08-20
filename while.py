79aad4556e7f3fc93af49a615d09b8c8239cc8e0
sreeharipremanathan <sreeharipremanathan@gmail.com>
2024-08-20 13:39:40 +0530

    python

--- while.py
+++ while.py
@@ -44,7 +44,7 @@
 #     i+=1
 
 # sum of even numbers
-
+# ---------------------
 # i=int(input('enter the starting number:'))
 # end=int(input('enter the ending number:'))
 # sum=0
@@ -55,7 +55,7 @@
 # print('the sum is',sum)
 
 # sum of odd numbers
-
+# ---------------------
 # i=int(input('enter the starting number:'))
 # end=int(input('enter the ending number:'))
 # sum=0
@@ -67,7 +67,7 @@
 
 
 # multiplication table
-
+# ----------------------
 # i=1
 # num=int(input('enter the value : '))
 # value=0
@@ -76,19 +76,68 @@
 #     print(i,'*',num,'=',value)
 #     i+=1
 
-i=int(input('enter the starting: '))
-end=int(input('enter the ending: '))
-sum=0
-even=0
-odd=0
-while i<=end:
-    sum=sum+i
-    if i%2==0:
-        even=even+i
-    else:
-        odd=odd+i
-    i+=1
-print('the sum of natural number',sum)
-print('the sum of even numbers',even)
-print('the sum of odd numbers',odd)
+# print sum of natural numbers,odd,even numbers
+# -------------------------------
+# i=int(input('enter the starting: '))
+# end=int(input('enter the ending: '))
+# sum=0
+# even=0
+# odd=0
+# while i<=end:
+#     sum=sum+i
+#     if i%2==0:
+#         even=even+i
+#     else:
+#         odd=odd+i
+#     i+=1
+# print('the sum of natural number',sum)
+# print('the sum of even numbers',even)
+# print('the sum of odd numbers',odd)
 
+# factiorial of a number
+# ------------------------
+# i=1
+# num=int(input('enter the number :'))
+# fact=1
+# while i<=num:
+#     fact*=i
+#     i+=1
+# print('factorial is:',fact)
+
+# to print a number in reverse
+# ------------------------------
+# num=int(input('enter the number :'))
+# rev=0
+# while num>0:
+#     t=num%10
+#     rev=rev*10+t
+#     num//=10
+# print(rev)
+
+# to find the sum of digits
+# ---------------------------
+# num=int(input('enter the number :'))
+# rev=0
+# while num>0:
+#     t=num%10
+#     rev=rev+t
+#     num//=10
+# print(rev)
+
+# to print characters of a string
+
+# s=input('enter a word: ')
+# l=len(s)
+# i=0
+# while i<l:
+#     print(s[i])
+#     i+=1
+
+str=input('enter a string: ')
+l=len(str)
+rev=""
+i=0
+while i<l:
+    rev=str[i]+rev
+    i+=1
+print(rev)
