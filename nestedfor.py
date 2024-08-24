2c436ca67e26fbc667dcaadcc378f5da2862e757
sreeharipremanathan <sreeharipremanathan@gmail.com>
2024-08-24 13:38:22 +0530

    nested for loop

---
+++ nestedfor.py
@@ -0,0 +1,91 @@
+# for i in range(3):
+#     print(i)
+#     for j in range(4):
+#         print('\t',j)
+
+# in matrix form
+# --------------
+
+# for i in range(3):
+#     for j in range(3):
+#         print(j,end="\t")
+#     print()
+    
+
+
+# to print a 3x3 matrix with values 10,11,12
+# ------------------------------------------
+
+# for i in range(3):
+#     a=10
+#     for j in range(3):
+#         print(a,end="\t")
+#         a+=1
+#     print()
+
+
+# to print a 7x5 matrix with values 51,52,53,54,55
+# ------------------------------------------------
+
+# for i in range(7):
+#     a=51
+#     for j in range(5):
+#         print(a,end="\t")
+#         a+=1
+#     print()
+# output
+# 51      52      53      54      55
+# 51      52      53      54      55
+# 51      52      53      54      55
+# 51      52      53      54      55
+# 51      52      53      54      55
+# 51      52      53      54      55
+# 51      52      53      54      55
+
+# for i in range(3):
+#     for j in range(3):
+#         print(i,end="\t")
+#     print()
+
+# out put
+# --------
+# 0   0   0
+# 1   1   1
+# 2   2   2
+
+
+# for i in range(3):
+#     for j in range(3):
+#         print(i,end="\t")
+#         i+=1
+#     print()
+
+# output
+# -------
+# 0       1       2
+# 1       2       3
+# 2       3       4
+
+# a=0
+# for i in range(3):
+#     for j in range(3):
+#         print(a,end="\t")
+#         a+=1
+#     print()
+
+# output
+# -------
+# 0       1       2
+# 3       4       5
+# 6       7       8
+
+
+# for i in range(4):
+#     if i%2==0:
+#         for j in range(3):
+#             print(j,end="\t")
+#     else:
+#             for j in range(3):
+#                 print(2-j,end="\t")
+#     print()
+
