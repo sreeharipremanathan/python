632525fd5bcbc35547b58a349f870999627fbdb3
sreeharipremanathan <sreeharipremanathan@gmail.com>
2024-08-28 16:16:00 +0530

    list

---
+++ list.py
@@ -0,0 +1,73 @@
+# l=[11,24,'abc',3.4]
+# for i in l:
+#     print(i)
+
+
+# if 10 in l:
+#     print("avil")
+# else:
+#     print("not avil")
+# print(l)
+# print(l[2])
+# updated abc into another value
+# l[2]="hari"
+# print(l[2])
+# l[1]="hey"
+# print(l)
+# methods to add
+
+# l.append([1,2,3])
+# l.append("hey there!")
+
+# l.extend(['hello',"hari",18])
+
+# l.insert(2,"i'm")
+# print(l)
+# print(l[5][2])  
+
+# methods to delete
+# l.clear()
+# print(l)
+
+# l.pop()
+# print(l)
+
+# l.pop(0)
+# print(l)
+
+# l.remove("hello")
+# print(l)
+
+# l.insert(-1,'hello')
+# print(l)
+
+# l=[7,8,9,5,3,22,1]
+# l.sort()
+# print(l)
+# l.reverse()
+# print(l)
+
+# l1=l
+# print(id(l))
+# print(id(l1))
+# print(l)
+# print(l1)
+# l1.pop()
+# print(l)
+# print(l1)
+
+# l1=l.copy()
+# print(id(l))
+# print(id(l1))
+# l1.pop()
+# print(l)
+# print(l1)
+
+
+# print(l.index(22))
+
+# tsk
+l=[[7,8,9,5,3,22,1]]
+for i in l:
+    if i%2==0:
+        print(i)
