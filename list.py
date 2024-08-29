c0d975b68e02b37999151e2c7f7e5980081fe13e
sreeharipremanathan <sreeharipremanathan@gmail.com>
2024-08-29 13:29:36 +0530

    python

--- list.py
+++ list.py
@@ -67,10 +67,67 @@
 # print(l.index(22))
 
 # tsk
-l=[7,8,9,5,2,4,3,22,1]
-sum=0
-for i in l:
-    if i%2==0:
-        print(i)
-        sum+=i
-print("sum of even numbers is= ",sum)
+# l=[7,8,9,5,2,4,3,22,1]
+# sum=0
+# for i in l:
+#     if i%2==0:
+#         print(i)
+#         sum+=i
+# print("sum of even numbers is= ",sum)
+
+# l=['hello','welcome','hari']
+# l.reverse()
+# print(l)
+# for i in l:
+#     print(i[::-1])
+
+# l=[1,2,4.5,'hey']
+# sum=0
+# for i in l:
+#     print(type(i))
+#     if type(i)==int or type(i)==float:
+#         sum+=i
+# print(sum)
+
+# l=[2,4,5,4,2,5,6,7]
+# l1=[]
+# for i in l:
+#     if i not in l1:
+#         l1.append(i)
+# print(l1)
+
+# names=[]
+# limit=int(input("enter the number of names: "))
+# for i in range(limit):
+#     name=input('enter the name: ')
+#     names.append(name)
+# print(names)
+
+# names=[]
+# limit=int(input("enter the number of names: "))
+# for i in range(limit):
+#     name=input('enter the name: ')
+#     age=int(input('enter the age: '))
+#     mark=int(input('enter the mark: '))
+#     names.append([name,age,mark])
+# print(names)
+
+# student management system
+# -------------------------
+std=[]
+while True:
+    print('''
+        1.add std
+        2.view std
+        3.update std
+        4.delete std''')
+    choice=int(input('please choose an option: '))
+    if choice==1:
+        name=input('enter the name: ')
+        age=int(input('enter the age: '))
+        mark=int(input('enter the mark: '))
+        std.append([name,age,mark])
+    elif choice==2:
+        print('{:<10}{:<5}{:<5}'.format('name','age','mark'))
+        for i in std:
+            print('{:<10}{:<5}{:<5}'.format(i[0],i[1],i[2]))
