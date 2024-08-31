ad008c367ec8b68dd2e39d684d2f9e969177d70d
sreeharipremanathan <sreeharipremanathan@gmail.com>
2024-08-31 12:50:59 +0530

    epm

--- ems.py
+++ ems.py
@@ -1,3 +1,5 @@
+
+import datetime
 emp=[]
 while True:
     print('''
@@ -18,7 +20,7 @@ while True:
         exp=int(input('Enter your experience: '))
         emp.append([id,name,age,poition,salary,exp])
     elif choice==2:
-        print('{:<5}{:<10}{:<5}{:<10}{:<10}{:<5}'.format('id','Name','Age','position','salary','Experience'))
+        print('{:<5}{:<10}{:<5}{:<15}{:<10}{:<5}'.format('id','Name','Age','position','salary','Experience'))
         for i in emp:
             print('{:<5}{:<10}{:<5}{:<10}{:<10}{:<5}'.format(i[0],i[1],i[2],i[3],i[4],i[5]))
     elif choice==3:
@@ -26,23 +28,26 @@ while True:
         f=0
         for i in emp:
             if i[0]==id:
-                print('''1.age \n 2.position \n 3.salary \n 4.experience''')
-                ch=int(input('choose any option :'))
-                if ch==1:
-                    age=int(input("enter updated age: "))
-                    i[2]=age
-                elif ch==2:
-                    poition=input('enter updated position: ')
-                    i[3]=poition
-                elif ch==3:
-                    salary=input('enter updated salary: ')
-                    i[4]=salary
-                elif ch==4:
-                    exp=int(input('enter updated experience: '))
-                    i[5]=exp
-                else:
-                    print('please choose an valid option :')
                 f=1
+                while True:
+                    print('''1.age \n 2.position \n 3.salary \n 4.experience \n 5.exit''')
+                    ch=int(input('choose any option :'))
+                    if ch==1:
+                        age=int(input("enter updated age: "))
+                        i[2]=age
+                    elif ch==2:
+                        poition=input('enter updated position: ')
+                        i[3]=poition
+                    elif ch==3:
+                        salary=input('enter updated salary: ')
+                        i[4]=salary
+                    elif ch==4:
+                        exp=int(input('enter updated experience: '))
+                        i[5]=exp
+                    elif ch==5:
+                        break
+                    else:
+                        print('please choose an valid option :')
         if f==0:
             print('invali id!!!')
     elif choice==4:
@@ -63,6 +68,17 @@ while True:
                 f=1
         if f==0:
             print('employee not found')
+    elif choice==6:
+        id=int(input('enter id of the employee: '))
+        f=0
+        for i in emp:
+            if i[0]==id:
+                f=1
+                task=input('enter the task: ')
+                date=datetime.datetime.now().strftime("%x")
+                i.append([task,date])
+        if f==0:
+            print('invalid id!!!')
     elif choice==7:
         break
     else:
