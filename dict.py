57af4fec532b74fdf110c1223ffb2037030fad36
sreeharipremanathan <sreeharipremanathan@gmail.com>
2024-09-02 13:36:49 +0530

    dict

---
+++ dict.py
@@ -0,0 +1,54 @@
+# d={'name':'hari','age':22,'place':'tsr'}
+# print(d)
+# print(d['name'])
+# d['place']='ekm'
+# d['mark']=88
+# print(d)
+# for i in d:
+    # print(i)
+
+    # name
+    # age
+    # place
+    # mark
+    # print(d[i])
+
+    # hari
+    # 22
+    # ekm
+    # 88
+
+    # print(i,d[i])
+    
+    # name hari
+    # age 22
+    # place ekm
+    # mark 88
+
+# if d['name']=='athi':
+#     print('true')
+# else:
+#     print('false')
+# if 'mark' in d:
+#     print('true')
+# else:
+#     print('false')
+
+# print(d.get('name'))
+# print(d.get('skill'))
+# print(d.values())
+# print(d.keys())
+# print(d.values())
+# d.pop('place')
+# print(d)
+# d.popitem()
+# print(d)
+# d.update({'na
+
+d={}
+# l=['name','age','mark','place']
+# d=d.fromkeys(l)
+# print((d))
+
+d.setdefault('name')
+print(d)
