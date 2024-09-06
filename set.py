566e584f02417fb00ef52ecb3dd38a9143692d52
sreeharipremanathan <sreeharipremanathan@gmail.com>
2024-09-06 13:34:59 +0530

    tuple,set

---
+++ set.py
@@ -0,0 +1,29 @@
+# s={1,3,52,'abc',3,2,4,5}
+# print(s)
+
+# to get empty set
+# s=set()
+
+# remove duplicates using set
+# ---------------------------
+# l=[22,33,11,55,10,55,22,11,33]
+# s=set(l)
+# print(s)
+# l=list(s)
+# print(l)
+
+s={1,3,2}
+# s.add(5)
+# print(s)
+s1={1,3,7}
+# print(s.difference(s1))
+# s1.clear()
+# print(s1)
+# s.discard(3)
+# print(s)
+# s.remove(3)
+# print(s)
+# s.pop()
+# print(s)
+s.update({5,6,7})
+print(s)
---
+++ tuple.py
@@ -0,0 +1,56 @@
+# t=(1,4,'hey',5.6,'hello')
+# for i in t:
+#     print(i)
+
+
+# if 4 in t:
+#     print('true')
+# else:
+#     print('false')
+
+
+# print(t[2])
+
+# t=1,2,3,'hey'
+# print(t)
+# print(type(t))
+'''(1, 2, 3, 'hey')
+<class 'tuple'>'''
+
+# t=16,
+# print(t)
+# print(type(t))
+'''(16,)
+<class 'tuple'>'''
+
+# t=(1,2,3,[11,23])
+# t[3].append(33)
+# print(t)
+'''(1, 2, 3, [11, 23, 33])'''
+
+# t=[1,2,3,'hey',(2,4)]
+
+
+# t=(1,2,3,4,2)
+# print(t.index(2))
+# print(t.count(2))
+
+
+# t=(1,2,3,4)
+# print(t)
+# l=list(t)
+# l.pop()
+# print(l)
+# t=tuple(l)
+# print(t)
+'''
+(1, 2, 3, 4)
+[1, 2, 3]
+(1, 2, 3)'''
+
+
+# d={'name':'hari','age':22}
+# l=list(d)
+# print(type(l))
+# print(l)
+
